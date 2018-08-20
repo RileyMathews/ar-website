f87bbf3e044570a38c840c12414c7c53e2680c8a
Riley Mathews <rileymathews80@gmail.com>
2018-08-19 21:25:51 -0500

    cleared warnings

--- src/views/Music.js
+++ src/views/Music.js
@@ -13,7 +13,7 @@ class Music extends Component {
                         <Title>
                             Music
                         </Title>
-                        <iframe width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/511451817&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+                        <iframe title="soundcloud" width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/511451817&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
                     </Container>
                 </HeroBody>
             </Hero>
