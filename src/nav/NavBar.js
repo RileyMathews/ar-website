cca534e59f1ee3bf3bacf6d37d154580c19680a7
Riley Mathews <rileymathews80@gmail.com>
2018-08-19 21:16:57 -0500

    got navbar jumping to elements

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -13,10 +13,17 @@ class NavBar extends Component {
         isActive: false,
     }
 
-    onClickNav = function () {
+    onClickNav = function (e) {
         this.setState({
             isActive: (!this.state.isActive)
         })
+        const navTarget = e.target.id.split("_")[1]
+        console.log(navTarget)
+
+        if (navTarget) {
+            const targetElement = document.getElementById(navTarget)
+            targetElement.scrollIntoView({behavior: "smooth"})
+        }
     }.bind(this)
 
     render() {
@@ -27,11 +34,11 @@ class NavBar extends Component {
                     <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
                 </NavbarBrand>
                 <NavbarMenu isActive={this.state.isActive}>
-                    <NavbarItem>Home</NavbarItem>
-                    <NavbarItem>Music</NavbarItem>
-                    <NavbarItem>Tour</NavbarItem>
-                    <NavbarItem>Instagram</NavbarItem>
-                    <NavbarItem>Contact</NavbarItem>
+                    <NavbarItem onClick={this.onClickNav} id="nav_banner">Home</NavbarItem>
+                    <NavbarItem onClick={this.onClickNav} id="nav_music">Music</NavbarItem>
+                    <NavbarItem onClick={this.onClickNav} id="nav_calendar">Tour</NavbarItem>
+                    <NavbarItem onClick={this.onClickNav} id="nav_social">Instagram</NavbarItem>
+                    <NavbarItem onClick={this.onClickNav} id="nav_contact">Contact</NavbarItem>
                 </NavbarMenu>
                 <NavbarEnd>
                     <NavbarItem isHidden="touch">{this.props.gamertag}</NavbarItem>
